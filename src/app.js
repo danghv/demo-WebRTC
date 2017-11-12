2fa9f36a900ed04e8e1d09bc72d65061d72d8426
danghv <hadangktmt@gmail.com>
2017-11-12 12:47:12 +0700

    finished

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
-const openCamera = require('./openCamera')
-openCamera();
-console.log('Hello');
+const Peer = require('simple-peer')
+const $ = require('jquery')
+const playVideo = require('./playVideo')
+const openStream = require('./openStream')
+openStream(function(stream) {
+    playVideo(stream, 'localStream');
+    const p = new Peer({ initiator: location.hash === '#1', trickle: false, stream:stream })
+    p.on('signal', token =>{
+        $('#txtMySignal').val(JSON.stringify(token))
+    })
+    $('#btnConnect').click(() => {
+        const friendSignal = JSON.parse($('#txtFriendSignal').val());
+        p.signal(friendSignal)
+    })
+    p.on('stream', friendStream => playVideo(friendStream, 'friendStream'))
+});
