b327670f96521ff9a8bf3d0bf77b2b7036bf43f5
danghv <hadangktmt@gmail.com>
2017-11-12 13:48:49 +0700

    make it better

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.listen(3000, () => console.log('Server started!'))
+app.listen(process.env.PORT || 3000, () => console.log('Server started!'))
